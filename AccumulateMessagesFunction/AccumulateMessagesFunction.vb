0e379c4208d11b03976662bc5803f734ec23cf98
Sebastian Soto <thefirstcoolone@gmail.com>
2021-09-21 22:55:07 -0600

    Add project files.

---
+++ AccumulateMessagesFunction/AccumulateMessagesFunction.vb
@@ -0,0 +1,13 @@
+﻿'Sebastian Soto
+'RCET0265
+'Fall 2021
+'_projectName
+'_gitURL
+Option Strict On
+Option Explicit On
+Module AccumulateMessagesFunction
+    Sub Main()
+
+    End Sub
+
+End Module
