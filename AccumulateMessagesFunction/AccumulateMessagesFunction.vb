79a0dd7c6175e1208fec32897652ee969d0c9e4a
Sebastian Soto <thefirstcoolone@gmail.com>
2021-09-25 21:58:18 -0600

    save

--- AccumulateMessagesFunction/AccumulateMessagesFunction.vb
+++ AccumulateMessagesFunction/AccumulateMessagesFunction.vb
@@ -7,7 +7,34 @@ Option Strict On
 Option Explicit On
 Module AccumulateMessagesFunction
     Sub Main()
+        Dim newMessage As String
+        Dim clear As Boolean
+        Dim quantity As Integer
+        Dim forUser(quantity) As String
 
+        UserMessages(newMessage, clear)
+
+        Do
+            quantity = quantity + 1
+            For i = LBound(forUser) To UBound(forUser)
+
+                Console.WriteLine(forUser(quantity))
+
+
+            Next
+        Loop Until clear = True
+        Console.ReadLine()
     End Sub
+    Function UserMessages(ByVal newMessage As String, ByVal clear As Boolean) As String
 
+        newMessage = Console.ReadLine()
+
+
+        If newMessage = "clear" Then
+            clear = True
+            newMessage = Nothing
+        End If
+
+        Return newMessage
+    End Function
 End Module
