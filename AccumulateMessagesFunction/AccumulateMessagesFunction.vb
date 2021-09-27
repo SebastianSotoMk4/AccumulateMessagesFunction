e42e0e7ba08b8961f6ebb9a683683241fd602a27
Sebastian Soto <thefirstcoolone@gmail.com>
2021-09-26 20:55:27 -0600

    save

--- AccumulateMessagesFunction/AccumulateMessagesFunction.vb
+++ AccumulateMessagesFunction/AccumulateMessagesFunction.vb
@@ -8,25 +8,35 @@ Option Explicit On
 Module AccumulateMessagesFunction
     Sub Main()
         Dim newMessage As String
+
         Dim clear As Boolean
-        Dim message As String = UserMessages(newMessage, clear)
+        Dim quit As Boolean
 
-        Console.WriteLine(message)
+        Dim message As String
+        Dim storage As String
+        Do Until quit = True
 
-
+            Console.WriteLine("Type a message for the user:")
+            UserMessages(newMessage, clear, storage)
+            If UserMessages(newMessage, clear, storage) = "quit" Then
+                quit = True
+            End If
+        Loop
 
         Console.ReadLine()
     End Sub
-    Function UserMessages(ByVal newMessage As String, ByVal clear As Boolean) As String
-
+    Function UserMessages(ByVal newMessage As String, ByVal clear As Boolean, ByVal storage As String) As String
+        Dim storeStr As String = newMessage + vbNewLine + storage
         newMessage = Console.ReadLine()
 
         If newMessage = "clear" Then
+            storeStr = Nothing
 
-            clear = True
-            newMessage = Nothing
+        ElseIf newMessage = "show" Then
+            storage = newMessage
+
         End If
 
-        Return newMessage
+        Return storage
     End Function
 End Module
